b71fa05e710da87f1bd84626fc3511d6017573e0
Jeff Shapiro <MacArthur1@MacArthur-Jeffs-MacBook-Pro.local>
2015-05-03 15:44:13 -0400

    hw completed

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var main = function() {
-  
+	
+	$('.day').click(function () {
+		console.log("day clicked");
+		// $('.hourly').toggle();
+		$(this).next().toggle();
+		$(this).find('span').toggleClass('glyphicon-minus');
+	});
+
+
 };
 
 $(document).ready(main);
